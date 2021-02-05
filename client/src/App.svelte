7ab134171c32018770d6c018cb464ac477e44b48
Oriol <magna.OMIRO.shogun@gmail.com>
2021-02-05 19:57:42 +0100

    Add Smelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,42 @@
 <script lang="ts">
-	export let name: string;
+  import 'smelte/src/tailwind.css';
+
+  import Button from "smelte/src/components/Button";
+
+  export let name: string;
+
+  function handler() {
+	  console.log('handled');
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+  <div class="py-2">
+	<Button light on:click={handler}>Button</Button>
+  </div>
+</main>
