24f186017eb73897af36dd4c957ce72b7587ab96
Oriol <magna.OMIRO.shogun@gmail.com>
2021-09-11 11:59:13 +0200

    Added Pieces Left component

---
+++ client/src/components/PiecesLeft.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { Piece } from '../data/piece.enum';
+  import { gameState, ownTurn, pieceSelected, piecesLeft } from '../stores/game.store';
+  import { sendState } from '../stores/websocket.store';
+
+  const pieces = Object.values(Piece);
+
+  $: selectable = $ownTurn && $pieceSelected == null;
+
+  function selectPiece(piece: Piece) {
+    if (selectable) {
+      gameState.selectPiece(piece);
+      sendState();
+    }
+  }
+</script>
+
+<style>
+  .content {
+    display: grid;
+    grid-template-columns: repeat(2, 100px);
+    gap: 0.25rem;
+  }
+
+  .cell {
+    width: 100px;
+    height: 100px;
+  }
+
+  .selectable.cell:hover {
+    cursor: pointer;
+    --tw-bg-opacity: 0.8;
+  }
+
+  @media (min-width: 1280px) {
+    .content {
+      grid-template-columns: repeat(2, 150px);
+      gap: 0.5rem;
+    }
+
+    .cell {
+      width: 150px;
+      height: 150px;
+    }
+  }
+</style>
+
+<div class="h-full overflow-auto justify-center content">
+  {#each pieces as piece}
+    <div class="bg-blue-50 border-2 border-blue-900 rounded cell" class:selectable>
+      {#if $piecesLeft.includes(piece)}
+        <div class="w-full h-full flex justify-center items-center" on:click={() => selectPiece(piece)}>
+          {piece}
+        </div>
+      {/if}
+    </div>
+  {/each}
+</div>
--- client/src/pages/Game.svelte
+++ client/src/pages/Game.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Button from 'smelte/src/components/Button';
+import PiecesLeft from '../components/PiecesLeft.svelte';
 
   import { WSKind } from '../data/websocket-kind.enum';
   import { gameState, ownTurn } from '../stores/game.store';
@@ -30,6 +31,26 @@
       url(https://fonts.googleapis.com/css2?family=Orbitron&display=swap) format('truetype');
     font-weight: 400 900;
   }
+
+  .header {
+    height: 5%;
+  }
+
+  .content {
+    height: 95%;
+  }
+
+  .selected-piece-area {
+    flex: 1;
+  }
+
+  .game-area {
+    flex: 2;
+  }
+
+  .pieces-left-area {
+    flex: 1;
+  }
 </style>
 
 {#if $errorWS?.kind === WSKind.Join}
@@ -39,7 +60,7 @@
     <Button light class="mt-4" href="/">Go Back</Button>
   </div>
 {:else}
-  <div class="flex flex-col justify-center items-center">
+  <!-- <div class="flex flex-col justify-center items-center">
     <div>This is the Game {roomKey}</div>
     <div>Game state: {JSON.stringify($gameState, undefined, 2)}</div>
     <Button on:click={restart} disabled={!$ownTurn}>Restart</Button>
@@ -49,5 +70,15 @@
     {#if $concedeWS}
       <div>You win!</div>
     {/if}
+  </div> -->
+  <header class="w-full bg-blue-500 rounded flex justify-center items-center text-gray-50 text-lg font-medium header">
+    Game {roomKey}
+  </header>
+  <div class="flex content">
+    <div class="bg-gray-500 h-full p-2 selected-piece-area"></div>
+    <div class="bg-blue-200 h-full p-2 game-area"></div>
+    <div class="h-full p-2 pieces-left-area">
+      <PiecesLeft></PiecesLeft>
+    </div>
   </div>
 {/if}
